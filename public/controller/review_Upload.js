9ad5e3b1399e1cb8d4967faf0031b72808c5a25a
leechanjun <thejunys@naver.com>
2016-11-11 18:03:29 +0900

    정규표현
    정규표현

--- public/controller/review_Upload.js
+++ public/controller/review_Upload.js
@@ -1,14 +1,39 @@
 myApp.controller("review_Upload",function($scope,$http,$location){  //review 컨트롤러 기능
 	$scope.viewDate ={}; //post 사용 시 값보냄
 	$scope.viewWriteForm = function($event){ 
-    $http({
-  	  		url:"/viewWrite",
-  	  		method:"post",
-  	  		data:$scope.viewDate
-  	  	}).success(function(){
-          alert(data);
-  	  	});
-  	};
+    var reg_Name = /^[a-zA-Z가-힣\s]{1,15}$/;
+    var result_Name = reg_Name.test($('#name').val());
+    var reg_age = /^[1-9][0-9]{0,2}$/;
+    var result_age = reg_age.test($('#age').val());
+    var reg_content = /^[a-zA-Z가-힣\s\W]{1,20}$/;
+    var result_content = reg_content.test($("#content").val());
+    if(!result_Name){
+      alert("이름을 적어주세요.");
+      $event.preventDefault(); 
+    }
+    else if(!result_age){
+      alert("나이를 적어주세요.");
+      $event.preventDefault();
+    }
+    else if($("#upload_").val()==""){
+      alert("파일을 선택 해주세요.");
+      $event.preventDefault();
+      
+    }
+    else if(!result_content){
+      alert("인사말을 적어주세요");
+       $event.preventDefault();
+    }
+    else{
+      $http({
+    	  		url:"/viewWrite",
+    	  		method:"post",
+    	  		data:$scope.viewDate
+    	  	}).success(function(){
+            alert(data);
+    	  	});
+    	}
+    };
 	angular.element(function(){              // input file css 기능 script
 		var fileTarget = $('.filebox .upload-hidden');
     	fileTarget.on('change', function(){  // 값이 변경되면
